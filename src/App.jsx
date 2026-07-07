9929deb4b4915c35f5b85e64467f75c8566692bc
Nooreldin Abdelnaser <nbdalnasr52@gmail.com>
2026-07-07 15:52:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import "./app.css";
 import Home from "./Pages/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <Routes>
-        <Route path="./" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
